0cbd98a75891b342be81ebe5878cfd672314c862
AgustinLaValla <agulavalla@gmail.com>
2023-06-02 12:52:49 -0300

    Add HttpJsonBodyParser as middleware

--- src/addTask.js
+++ src/addTask.js
@@ -1,11 +1,15 @@
 const { v4: uuid } = require("uuid");
 const { DynamoDB } = require("aws-sdk");
 
+const middy = require('@middy/core');
+const jsonBodyParser = require("@middy/http-json-body-parser");
+
+
 const addTask = async (event) => {
   // Create DB
   const dynamoDB = new DynamoDB.DocumentClient();
 
-  const { title, description } = JSON.parse(event.body);
+  const { title, description } = event.body;
   const date = new Date().toISOString();
   const id = uuid();
 
@@ -24,4 +28,4 @@ const addTask = async (event) => {
   };
 };
 
-module.exports = { addTask };
+module.exports = { addTask: middy(addTask).use(jsonBodyParser()) };
